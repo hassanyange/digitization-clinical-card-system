03dcdd8403fd25e14d3df0b1ece10e11bb1a3b29
benny-png <mazikuben2@gmail.com>
2024-07-03 09:44:09 +0300

    some changes

--- main/models.py
+++ main/models.py
@@ -55,8 +55,9 @@ class Appointment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    
     def __str__(self):
-        return self.first_name
+        return f"Appointment with {self.appointment_with} on {self.appointment_date}"
 
 class Patient(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
